0768605ea641aa02ea78ba1f8ed453b2e33fee2e
Stevie Robinson <stevie.robinson@gmail.com>
2025-07-08 18:15:31 +0200

    move mock definitions to test and add failsafe test

--- src/NzbDrone.Core/Datastore/Migration/219_nzb_su_url_to_nzb_life.cs
+++ src/NzbDrone.Core/Datastore/Migration/219_nzb_su_url_to_nzb_life.cs
@@ -1,6 +1,4 @@
-using System.Collections.Generic;
 using FluentMigrator;
-using Newtonsoft.Json.Linq;
 using NzbDrone.Core.Datastore.Migration.Framework;
 
 namespace NzbDrone.Core.Datastore.Migration
@@ -14,27 +12,5 @@ namespace NzbDrone.Core.Datastore.Migration
                         "WHERE \"Implementation\" = 'Newznab'" +
                         "AND \"Settings\" LIKE '%//api.nzb.su%'");
         }
-    }
-
-    public class IndexerDefinition219
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public JObject Settings { get; set; }
-        public int Priority { get; set; }
-        public string Implementation { get; set; }
-        public string ConfigContract { get; set; }
-        public bool EnableRss { get; set; }
-        public bool EnableAutomaticSearch { get; set; }
-        public bool EnableInteractiveSearch { get; set; }
-        public HashSet<int> Tags { get; set; }
-        public int DownloadClientId { get; set; }
-        public int SeasonSearchMaximumSingleEpisodeAge { get; set; }
-    }
-
-    public class NewznabSettings219
-    {
-        public string BaseUrl { get; set; }
-        public string ApiPath { get; set; }
     }
 }
